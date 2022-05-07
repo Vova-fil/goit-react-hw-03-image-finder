1b880958fc7415f2e794b4331f29d7e51b3c9e10
Vova-fil <vfil.zp@gmail.com>
2022-05-08 01:18:19 +0300

    add модалку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+export default class App extends Component {
+  state = {
+    searchImage: '',
+    error: null,
+    status: 'idle',
+    images: [],
+    page: 1,
+  };
+
+   onButtonClick() {
+    
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  }
+   render() {
+    const { images, status, error } = this.state;
+     return (
+       {status === 'resolved' && (
+          <div>
+            <ButtonLoadMore onClick={() => this.onButtonClick()} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         textTransform: 'uppercase',
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
